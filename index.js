c094b883f8c0fc09e55190391ad7ac09e3ff5460
Mark Howell <MHowellJr16@gmail.com>
2025-09-10 08:02:03 -0400

    Sep. 10__Update

---
+++ index.js
@@ -0,0 +1,13 @@
+// template_g613608
+// service_xtldnql
+// zBtD0yASn6CV5-gFr
+function contact() {
+    event.preventDefault();
+    emailjs
+        .sendForm(
+            'sevice_xtldnql',
+            'template_g613608',
+            event.target,
+            'zBtD0yASn6CV5-gFr'
+        )
+}
