0f0dbdce30da06df95785088d42c7e0fb7c65fe6
Mark Howell <MHowellJr16@gmail.com>
2025-09-12 12:18:03 -0400

    Sep. 12__Update

--- index.js
+++ index.js
@@ -2,7 +2,10 @@
 // service_xtldnql
 // zBtD0yASn6CV5-gFr
 function contact(event) {
-   /* event.preventDefault();
+event.preventDefault()
+const loading = document.querySelector('.modal__overlay--loading')
+const success = document.querySelector('.modal__overlay--success')
+loading.classList += " modal__overlay--visible" 
     emailjs
         .sendForm(
             'service_xtldnql',
@@ -10,16 +13,16 @@ function contact(event) {
             event.target,
             'zBtD0yASn6CV5-gFr'
         ).then(() => {
-            console.log("SUCCESS!");
-        }
+            loading.classList.remove("modal__overlay--visible")
+            success.classList += " modal__overlay--visible"
+        }).catch(() => {
+            loading.classList.remove("modal__overlay--visible")
+            alert(
+                "The email service is temporarily unavailable. Please contact me directly at"
         )
-    */ 
-   const loading = document.querySelector('.modal__overlay--loading');
-   const success = document.querySelector('.modal__overlay--success');
-   loading.classList += " modal__overlay--visible" 
-   setTimeout(() => {
-     loading.classList.remove("modal__overlay--visible")
-        success.classList += " modal__overlay--visible"
-     console.log("SUCCESS! 1")
-    }, 1000);
+    })
+    }
+
+function toggleModal() {
+    document.body.classList += " modal--open"
 }
