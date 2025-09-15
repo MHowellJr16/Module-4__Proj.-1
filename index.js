0263aefb56584e5ed959f308568f4080fc7d4051
Mark Howell <MHowellJr16@gmail.com>
2025-09-15 17:54:08 -0400

    Sep. 15__Update

--- index.js
+++ index.js
@@ -22,7 +22,12 @@ loading.classList += " modal__overlay--visible"
         )
     })
     }
-
+let isModalOpen = false
 function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = false
+        return document.body.classList.remove("modal--open")
+    }
+    isModalOpen = true
     document.body.classList += " modal--open"
 }
